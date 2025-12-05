1eb8212c60ca67a35b015b246c0db67e5b84c687
Ilaria <tuoindirizzo@email.com>
2025-12-05 14:41:52 +0000

    deploy app on Netlify

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const __dirname = path.dirname(__filename);
 // Middleware
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
-app.use(cors({ origin: "http://localhost:4200", credentials: true }));
+app.use(cors({ origin: 'https://wattwisee.netlify.app', credentials: true }));
 app.use(cookieParser());
 
 // Rotte API
