cf108260456500455648269bf01160966a5fa8b3
Ilaria <tuoindirizzo@email.com>
2025-12-16 14:22:34 +0000

    Remove angular-fontawesome wrapper (use CSS icons only)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,8 +4,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
